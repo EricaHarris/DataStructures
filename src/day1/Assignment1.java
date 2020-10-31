ee2298911ea2247914979e21221d5a77ed6713d6
McMillerErika <erikamcmiller65@gmail.com>
2020-10-30 22:21:10 -0400

    Assignment 1 solution

--- src/day1/Assignment1.java
+++ src/day1/Assignment1.java
@@ -1,4 +1,20 @@
 package day1;
 
+import java.util.Arrays;
+
 public class Assignment1 {
+    public static void main(String[] args) {
+        int[] integers = {1,2,3,4,5,6,7,8,9,10};
+        reverseArray(integers);
+    }
+
+    public static void reverseArray(int[] arr){
+
+        for (int i = 0; i < arr.length/2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length-i-1];
+            arr[arr.length-i-1] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
